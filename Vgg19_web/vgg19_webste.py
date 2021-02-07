604fa95cb5d5329ebfe864e2a9c3962faed353c2
faruk <frklmz09@gmail.com>
2021-02-07 06:45:48 +0300

    lfs2.commit

---
+++ Vgg19_web/vgg19_webste.py
@@ -0,0 +1,81 @@
+from PyQt5.QtWidgets import QApplication, QFileDialog
+from PyQt5.QtWidgets import *
+from PyQt5.uic import *
+from PyQt5.Qt import QApplication, QUrl, QDesktopServices
+import sys
+from PyQt5.QtGui import QPixmap
+from yolo_deneme import Getir_yolo
+import cv2
+import streamlit as st
+from PIL import Image, ImageOps
+import time
+
+class window():
+        def __init__(self):
+                
+                st.write("""
+                         # X-RAY IMAGE DETECTION
+                         """
+                         )
+                self.file = st.file_uploader("Please upload an image file", type=["jpg", "png","jpeg"])
+                
+               
+                image = Image.open(self.file)
+                image=image.resize((100,100))
+                st.image(image)
+                if(st.button("İşlem Yap")):
+                    self.sec(self.file)  
+                
+        def sec(self,file_name):
+             
+                self.photo_path = file_name 
+           
+                self.islem()
+      
+               
+               
+        def islem(self):
+                    start = time.time()
+                    import tensorflow as tf
+                  
+                    from keras.preprocessing import image
+                    from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+                    import numpy as np
+                    from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+                    from keras.models import Sequential
+                    from keras.layers import Dense
+                    from keras.applications.vgg16 import VGG16
+                    import matplotlib.pyplot as plt
+                    from glob import glob
+                    class_names =  ["Kovid","Normal","Zaturre"]
+                    try:
+                        model = tf.keras.models.load_model(r"./yolovgg19")
+                        self.img=Getir_yolo(self.photo_path)
+                        dim = (224, 224)      
+                        self.img = cv2.cvtColor(np.array(self.img), cv2.COLOR_RGB2BGR)
+                        resim_crop = cv2.resize(self.img, dim, interpolation = cv2.INTER_AREA)
+              
+                        image1 = self.img          
+                        dim = (224, 224)
+                        image1 = cv2.cvtColor(np.array(image1), cv2.COLOR_RGB2BGR)
+                        img = cv2.resize(image1, dim, interpolation = cv2.INTER_AREA)
+                        #img=image1.resize((224,224))
+                        img = image.img_to_array(img)
+                        img = np.expand_dims(img, axis=0)
+                        prediction = model.predict(img)
+                        
+                        prediction = np.argmax(prediction,axis=1)
+                        print(prediction)
+                        print(class_names[prediction[0]])
+                    
+                        end = time.time()
+                        fark = end - start   
+                        st.write("Tahmin Sonucu: "+str(class_names[prediction[0]]))
+                        st.write("Tahmin Süresi(sn): "+str(round(fark,2)))
+                    except:
+                        st.write("Ciğer Tespit Edilemedi")
+            
+if __name__ == "__main__":   
+    window = window()         
+           
+    
---
+++ Vgg19_web/yolo_deneme.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 15:06:55 2020
+
+@author: ASUS
+"""
+import cv2
+import numpy as np
+from PIL import Image, ImageOps
+def Getir_yolo(resim_yol):
+ 
+        img = Image.open(resim_yol)
+        
+        dim = (600, 600)
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)       
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+     
+        img_width = img.shape[1]
+        img_height = img.shape[0]
+        
+        img_blob = cv2.dnn.blobFromImage(img, 1/255, (416,416), swapRB=True, crop=False)
+        
+        labels = ["ciger","ciger2","ciger3"]
+        
+        
+        colors = ["0,255,255","0,0,255","255,0,0","255,255,0","0,255,0"]
+        colors = [np.array(color.split(",")).astype("int") for color in colors]
+        colors = np.array(colors)
+        # colors = np.tile(colors,(18,1))
+        
+        
+        model=cv2.dnn.readNetFromDarknet("./spot_yolov4.cfg","./spot_yolov4_final.weights")
+        
+        layers = model.getLayerNames()
+        output_layer = [layers[layer[0]-1] for layer in model.getUnconnectedOutLayers()]
+        
+        model.setInput(img_blob)
+        
+        detection_layers = model.forward(output_layer)
+        
+        ids_list=[]
+        boxes_list=[]
+        confidence_list=[]
+        
+        for detection_layer in detection_layers:
+            for object_detection in detection_layer:
+                
+                scores = object_detection[5:]
+                predicted_id = np.argmax(scores)
+                confidence = scores[predicted_id]
+                
+                if confidence > 0.20:
+                    
+                    label = labels[predicted_id]
+                    bounding_box = object_detection[0:4] * np.array([img_width,img_height,img_width,img_height])
+                    (box_center_x, box_center_y, box_width, box_height) = bounding_box.astype("int")
+                    
+                    start_x = int(box_center_x - (box_width/2))
+                    start_y = int(box_center_y - (box_height/2))
+                    
+                    ids_list.append(predicted_id)
+                    confidence_list.append(float(confidence))
+                    boxes_list.append([start_x, start_y,int(box_width),int(box_height)])
+                    
+            
+        crop_image= []
+        a=0
+        b=0
+        c=0
+        d=0
+        max_ids=cv2.dnn.NMSBoxes(boxes_list,confidence_list,0.5,0.4)
+        
+        for max_id in max_ids:
+            max_class_id=max_id[0]
+            box=boxes_list[max_class_id]
+            
+            start_x=box[0]
+            start_y=box[1]
+            box_width=box[2]
+            box_height=box[3]
+            
+            predicted_id=ids_list[max_class_id]
+            label=labels[predicted_id]
+            confidence=confidence_list[max_class_id]
+        
+        
+            end_x = start_x + box_width
+            end_y = start_y + box_height
+                    
+            box_color = colors[predicted_id]
+            box_color = [int(each) for each in box_color]
+                    
+                    
+            label = "{}: {:.2f}%".format(label, confidence*100)
+            print("predicted object {}".format(label))
+             
+              
+            cv2.rectangle(img, (start_x,start_y),(end_x,end_y),box_color,1)
+           
+            a=start_x
+            b=end_x
+            c=start_y
+            d=end_y
+            cv2.putText(img,label,(start_x,start_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
+            if a<0:
+                a=0
+            if b>img_width:
+                b=img_width
+            if c<0:
+                c=0
+            if d>img_height:
+                d=img_height
+            crop_image = img[c:d, a:b] 
+            # cv2.imshow("Detection Window", crop_image)
+            # cv2.waitKey(0)
+           
+        return crop_image  
+        # crop_image = img[a:b, c:d] 
+        # cv2.imshow("Detection Window", crop_image)
+        # cv2.waitKey(0)
+        
+        # cv2.imshow("Detection Window", img)
+        # cv2.waitKey(0)
+        cv2.destroyAllWindows()
+  
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
