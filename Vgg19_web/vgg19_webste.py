25de0e7d07e989e6e8197a911d1574a3c560c314
almazfaruk <46427829+almazfaruk@users.noreply.github.com>
2021-02-28 08:33:39 +0300

    Update vgg19_webste.py

--- Vgg19_web/vgg19_webste.py
+++ Vgg19_web/vgg19_webste.py
@@ -48,29 +48,29 @@ class window():
                     
                     class_names =  ["Kovid","Normal","Zaturre"]
                     #try:
-                        model = tf.keras.models.load_model("/Vgg19_web/yolovgg19")
-                        self.img=Getir_yolo(self.photo_path)
-                        dim = (224, 224)      
-                        self.img = cv2.cvtColor(np.array(self.img), cv2.COLOR_RGB2BGR)
-                        resim_crop = cv2.resize(self.img, dim, interpolation = cv2.INTER_AREA)
+                    model = tf.keras.models.load_model(r"./yolovgg19")
+                    self.img=Getir_yolo(self.photo_path)
+                    dim = (224, 224)      
+                    self.img = cv2.cvtColor(np.array(self.img), cv2.COLOR_RGB2BGR)
+                    resim_crop = cv2.resize(self.img, dim, interpolation = cv2.INTER_AREA)
               
-                        image1 = self.img          
-                        dim = (224, 224)
-                        image1 = cv2.cvtColor(np.array(image1), cv2.COLOR_RGB2BGR)
-                        img = cv2.resize(image1, dim, interpolation = cv2.INTER_AREA)
-                        #img=image1.resize((224,224))
-                        img = image.img_to_array(img)
-                        img = np.expand_dims(img, axis=0)
-                        prediction = model.predict(img)
+                    image1 = self.img          
+                    dim = (224, 224)
+                    image1 = cv2.cvtColor(np.array(image1), cv2.COLOR_RGB2BGR)
+                    img = cv2.resize(image1, dim, interpolation = cv2.INTER_AREA)
+                    #img=image1.resize((224,224))
+                    img = image.img_to_array(img)
+                    img = np.expand_dims(img, axis=0)
+                    prediction = model.predict(img)
                         
-                        prediction = np.argmax(prediction,axis=1)
-                        print(prediction)
-                        print(class_names[prediction[0]])
+                    prediction = np.argmax(prediction,axis=1)
+                    print(prediction)
+                    print(class_names[prediction[0]])
                     
-                        end = time.time()
-                        fark = end - start   
-                        st.write("Tahmin Sonucu: "+str(class_names[prediction[0]]))
-                        st.write("Tahmin Süresi(sn): "+str(round(fark,2)))
+                    end = time.time()
+                    fark = end - start   
+                    st.write("Tahmin Sonucu: "+str(class_names[prediction[0]]))
+                    st.write("Tahmin Süresi(sn): "+str(round(fark,2)))
                     #except:
                         #st.write("Ciğer Tespit Edilemedi")
             
