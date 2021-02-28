03f2b3f9dd7d08d55178285674167043f6187171
almazfaruk <46427829+almazfaruk@users.noreply.github.com>
2021-02-28 09:02:35 +0300

    Update vgg19_webste.py

--- Vgg19_web/vgg19_webste.py
+++ Vgg19_web/vgg19_webste.py
@@ -16,12 +16,12 @@ class window():
                          )
                 self.file = st.file_uploader("Please upload an image file", type=["jpg", "png","jpeg"])
                 
-               
-                image = Image.open(self.file)
-                image=image.resize((100,100))
-                st.image(image)
-                if(st.button("İşlem Yap")):
-                    self.sec(self.file)  
+                if self.file!=None:
+                      image = Image.open(self.file)
+                      image=image.resize((100,100))
+                      st.image(image)
+                      if(st.button("İşlem Yap")):
+                          self.sec(self.file)  
                 
         def sec(self,file_name):
              
@@ -47,32 +47,32 @@ class window():
                   
                     
                     class_names =  ["Kovid","Normal","Zaturre"]
-                    #try:
-                    model = tf.keras.models.load_model(r"Vgg19_web/yolovgg19")
-                    self.img=Getir_yolo(self.photo_path)
-                    dim = (224, 224)      
-                    self.img = cv2.cvtColor(np.array(self.img), cv2.COLOR_RGB2BGR)
-                    resim_crop = cv2.resize(self.img, dim, interpolation = cv2.INTER_AREA)
+                    try:
+                        model = tf.keras.models.load_model(r"Vgg19_web/yolovgg19")
+                        self.img=Getir_yolo(self.photo_path)
+                        dim = (224, 224)      
+                        self.img = cv2.cvtColor(np.array(self.img), cv2.COLOR_RGB2BGR)
+                        resim_crop = cv2.resize(self.img, dim, interpolation = cv2.INTER_AREA)
               
-                    image1 = self.img          
-                    dim = (224, 224)
-                    image1 = cv2.cvtColor(np.array(image1), cv2.COLOR_RGB2BGR)
-                    img = cv2.resize(image1, dim, interpolation = cv2.INTER_AREA)
-                    #img=image1.resize((224,224))
-                    img = image.img_to_array(img)
-                    img = np.expand_dims(img, axis=0)
-                    prediction = model.predict(img)
+                        image1 = self.img          
+                        dim = (224, 224)
+                        image1 = cv2.cvtColor(np.array(image1), cv2.COLOR_RGB2BGR)
+                        img = cv2.resize(image1, dim, interpolation = cv2.INTER_AREA)
+                        #img=image1.resize((224,224))
+                        img = image.img_to_array(img)
+                        img = np.expand_dims(img, axis=0)
+                        prediction = model.predict(img)
                         
-                    prediction = np.argmax(prediction,axis=1)
-                    print(prediction)
-                    print(class_names[prediction[0]])
+                        prediction = np.argmax(prediction,axis=1)
+                        print(prediction)
+                        print(class_names[prediction[0]])
                     
-                    end = time.time()
-                    fark = end - start   
-                    st.write("Tahmin Sonucu: "+str(class_names[prediction[0]]))
-                    st.write("Tahmin Süresi(sn): "+str(round(fark,2)))
-                    #except:
-                        #st.write("Ciğer Tespit Edilemedi")
+                        end = time.time()
+                        fark = end - start   
+                        st.write("Tahmin Sonucu: "+str(class_names[prediction[0]]))
+                        st.write("Tahmin Süresi(sn): "+str(round(fark,2)))
+                    except:
+                        st.write("Ciğer Tespit Edilemedi")
             
 if __name__ == "__main__":   
     window = window()         
